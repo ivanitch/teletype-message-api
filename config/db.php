afb90a746ec33f5cb8f858fab35ef0fd2f468da0
Sergey Ivanov <ivanitch.mail@gmail.com>
2025-06-19 19:34:33 +0900

    Подключение к БД

--- config/db.php
+++ config/db.php
@@ -1,9 +1,12 @@
 <?php
 
 return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
+    'class'               => 'yii\db\Connection',
+    'dsn'                 => 'pgsql:host=teletype_database;dbname=teletype',
+    'username'            => 'user',
+    'password'            => 'password',
+    'charset'             => 'utf8',
+    'enableSchemaCache'   => false,
+    'schemaCacheDuration' => false,
+    'schemaCache'         => false,
 ];
